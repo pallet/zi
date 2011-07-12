95acf8047fb2a3fcca1f2d108a2cd8c2d369f260
Hugo Duncan <hugo@hugoduncan.org>
2011-07-11 22:28:46 -0400

    Add a test goal for running clojure.test tests

--- src/main/clojure/zi/core.clj
+++ src/main/clojure/zi/core.clj
@@ -27,30 +27,46 @@
 
 (defn eval-clojure
   "Evaluate a closure form in a classloader with the specified paths"
-  [source-paths classpath-elements form]
-  (classlojure/eval-in
+  [source-paths classpath-elements form & args]
+  (apply
+   classlojure/eval-in
    (classloader-for (concat source-paths classpath-elements))
-   form))
+   form
+   args))
 
 (defn clj-files
   "Return a sequence of .clj files under the given base directory"
-  [base-directory]
+  [^String base-directory]
   {:pre [(.. (java.io.File. base-directory) isDirectory)]}
   (filter
-   #(and (.isFile %) (.. (.getName %) (.endsWith % ".clj")))
-   (file-seq base-directory)))
+   (fn [^java.io.File file]
+     (and (.isFile file) (.. (.getName file) (endsWith ".clj"))))
+   (file-seq (java.io.File. base-directory))))
 
 (defn file-to-namespace
   "Convert a filename to a namespace name"
   [filename]
-  (-> filename (string/replace "_" "-") (string/replace "/" ".")))
+  (-> filename
+      (string/replace #"\.clj$" "")
+      (string/replace "_" "-")
+      (string/replace "/" ".")))
 
 (defn find-namespaces
   [base-directory]
-  (map file-to-namespace (clj-files base-directory)))
+  (let [offset (inc (count base-directory))]
+    (->>
+     (clj-files base-directory)
+     (map #(subs (.getPath %) offset))
+     (map file-to-namespace))))
 
 (defn clojure-source-paths
   [source-directory]
   (if (.endsWith source-directory "/java")
     [(string/replace source-directory #"/java$" "/clojure") source-directory]
     [source-directory]))
+
+(defn path-on-classpath?
+  "Predicate to test whether a path matching the supplied regex is in the
+   classpath-elements"
+  [regex classpath-elements]
+  (some #(re-find regex %) classpath-elements))
---
+++ src/main/clojure/zi/test.clj
@@ -0,0 +1,86 @@
+(ns zi.test
+  "Run tests"
+  (:require
+   [zi.mojo :as mojo]
+   [zi.core :as core])
+  (:import
+   java.io.File
+   [clojure.maven.annotations
+    Goal RequiresDependencyResolution Parameter Component]
+   org.apache.maven.plugin.MojoExecutionException))
+
+(defn find-tests
+  "Find all clj files under the test source path, and return the
+   namespaces."
+  [source-paths]
+  (mapcat core/find-namespaces source-paths))
+
+(defn report-test
+  [log result]
+  (case (:type result)
+    :fail (.error log
+                  "FAIL %s expected: %s  actual: %s"
+                  (:message result "") (:expected result) (:actual result))
+    :error (.error log "%s expected: %s  actual: %s"
+                   (:message result "") (:expected result) (:actual result))))
+
+(defn run-tests
+  [classpath-elements test-source-directory log]
+  (let [cl (core/classloader-for classpath-elements)
+        bindings (gensym "bindings")
+        body (gensym "body")
+        test-ns-list (find-tests
+                      (core/clojure-source-paths test-source-directory))
+        test-ns-symbols (map #(list `quote (symbol %)) test-ns-list)]
+    (classlojure/eval-in
+     cl
+     `(do
+        (require '~'clojure.test)
+        (defmacro ~'redef [ [& ~bindings] & ~body ]
+          (if (find-var 'clojure.core/with-redefs)
+            `(with-redefs [~@~bindings] ~@~body)
+            `(binding [~@~bindings] ~@~body)))))
+    (let [results (classlojure/eval-in
+                   cl
+                   `(let [results# (atom [])
+                          original-report# clojure.test/report
+                          report# (fn [m#]
+                                    (original-report# m#)
+                                    (swap!
+                                     results# conj
+                                     (-> m#
+                                       (update-in
+                                        [:actual] #(when % (pr-str %)))
+                                       (update-in
+                                        [:ns] #(when %
+                                                 (list `quote (ns-name %)))))))]
+                      (~'redef
+                       [clojure.test/report report#]
+                       (binding [clojure.test/*test-out* *out*]
+                         (require ~@test-ns-symbols)
+                         (clojure.test/run-tests ~@test-ns-symbols)))
+                      @results#))
+          passes (dec (count (map :pass results)))
+          summary (last results)]
+      (.info
+       log
+       (format
+        "Tests run: %d, passed: %d, failed: %d, errors: %d"
+        (:test summary) (:pass summary) (:fail summary) (:error summary))))))
+
+(mojo/defmojo ClojureTest
+  {Goal "test"
+   RequiresDependencyResolution "test"}
+  [^{Parameter
+     {:expression "${clojure.test-ns}" :defaultValue ""
+      :description "List of namespaces to run tests for"}}
+   ^String
+   namespaces]
+
+  (run-tests
+   (concat
+    classpath-elements
+    (core/clojure-source-paths source-directory)
+    (core/clojure-source-paths test-source-directory)
+    (vec test-classpath-elements))
+   test-source-directory log))
